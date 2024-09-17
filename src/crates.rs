628c0805ac46141d05002fcfe0d0f981eb4e1734
nobel-von-it <maksimdavydenko12@gmail.com>
2024-09-17 10:33:30 +0700

    Add crates io integration

---
+++ src/crates.rs
@@ -0,0 +1,84 @@
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+pub struct FullCrateInfo {
+    #[serde(rename = "crate")]
+    crate_info: Crate,
+    pub versions: Vec<serde_json::Value>,
+}
+impl FullCrateInfo {
+    pub fn get_all_versions(&self) -> Vec<Version> {
+        let mut vs = vec![];
+        for v in self.versions.iter() {
+            if let Ok(v) = serde_json::from_value::<Version>(v.clone()) {
+                vs.push(v)
+            }
+        }
+        vs
+    }
+    pub fn get_features(&self, id: u64) -> Option<Vec<String>> {
+        if let Some(version) = self.get_version(id) {
+            return version.get_features();
+        }
+        None
+    }
+    pub fn get_version(&self, id: u64) -> Option<Version> {
+        if let Some(value) = self.versions.get(id as usize) {
+            if let Ok(version) = serde_json::from_value(value.clone()) {
+                return Some(version);
+            }
+        }
+        None
+    }
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+pub struct Crate {
+    name: String,
+    max_version: String,
+}
+#[derive(Serialize, Deserialize, Debug, Clone)]
+pub struct Version {
+    #[serde(rename = "crate")]
+    pub crate_name: String,
+    features: Option<serde_json::Value>,
+    pub num: String,
+}
+impl Version {
+    pub fn get_features(&self) -> Option<Vec<String>> {
+        if let Some(features) = &self.features {
+            return Some(features.as_object().unwrap().keys().cloned().collect());
+        }
+        None
+    }
+}
+
+pub struct CrateValidator {
+    pub name: String,
+    pub versions: Vec<String>,
+    pub features: Option<Vec<String>>,
+}
+
+impl CrateValidator {
+    pub fn get_last_from_cratesio(name: &str) -> Option<Self> {
+        let url = format!("https://crates.io/api/v1/crates/{}", name);
+        if let Ok(res) = reqwest::blocking::Client::new()
+            .get(url)
+            .header("User-Agent", "limp/0.1.0")
+            .send()
+        {
+            if let Ok(text) = res.json::<FullCrateInfo>() {
+                let versions = text.get_all_versions();
+                return Some(CrateValidator {
+                    name: text.crate_info.name.to_string(),
+                    versions: versions.iter().map(|v| v.num.to_string()).collect(),
+                    features: versions[0].get_features(),
+                });
+            }
+        }
+        None
+    }
+    pub fn validate(name: &str) -> bool {
+        CrateValidator::get_last_from_cratesio(name).is_some()
+    }
+}
