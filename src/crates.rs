4e256921b217309183452594e5dc4b8368f4f4d1
nobel-von-it <maksimdavydenko12@gmail.com>
2024-09-21 16:16:06 +0700

    Use lightweight ureq instead reqwest

--- src/crates.rs
+++ src/crates.rs
@@ -7,15 +7,14 @@ pub struct FullCrateInfo {
     versions: Vec<serde_json::Value>,
 }
 impl FullCrateInfo {
+    // TODO: rewrite to Result and use ?
     pub fn get_from_cratesio(name: &str) -> Option<Self> {
         let url = format!("https://crates.io/api/v1/crates/{}", name);
-        if let Ok(res) = reqwest::blocking::Client::new()
-            .get(url)
-            .header("User-Agent", "limp/0.1.0")
-            .send()
-        {
-            if let Ok(text) = res.json::<FullCrateInfo>() {
-                return Some(text);
+        if let Ok(res) = ureq::get(&url).set("User-Agent", "limp/0.1.2").call() {
+            if let Ok(body) = res.into_string() {
+                if let Ok(crate_info) = serde_json::from_str::<FullCrateInfo>(&body) {
+                    return Some(crate_info);
+                }
             }
         }
         None
@@ -65,58 +64,3 @@ impl Version {
         None
     }
 }
-
-// #[derive(Debug)]
-// pub struct CrateValidator {
-//     pub name: String,
-//     pub versions: Vec<String>,
-//     pub features: Option<Vec<String>>,
-// }
-//
-// impl CrateValidator {
-//     pub fn get_last_from_cratesio(name: &str) -> Option<Self> {
-//         let url = format!("https://crates.io/api/v1/crates/{}", name);
-//         if let Ok(res) = reqwest::blocking::Client::new()
-//             .get(url)
-//             .header("User-Agent", "limp/0.1.0")
-//             .send()
-//         {
-//             if let Ok(text) = res.json::<FullCrateInfo>() {
-//                 let versions = text.get_all_versions();
-//                 return Some(CrateValidator {
-//                     name: text.crate_info.name.to_string(),
-//                     versions: versions.iter().map(|v| v.num.to_string()).collect(),
-//                     features: versions[0].get_features(),
-//                 });
-//             }
-//         }
-//         None
-//     }
-//     pub fn validate(name: &str) -> bool {
-//         CrateValidator::get_last_from_cratesio(name).is_some()
-//     }
-//     pub fn dependency_validate(d: &Dependency) -> bool {
-//         if let Some(cv) = CrateValidator::get_last_from_cratesio(&d.name) {
-//             println!("{:#?}", &cv);
-//             println!("{:#?}", d);
-//             if cv.name == d.name && cv.versions.contains(&d.version) {
-//                 if d.features.is_none() {
-//                     return true;
-//                 } else if cv.features.is_some() && d.features.is_some() {
-//                     let cvf = cv.features.clone().unwrap();
-//                     let df = d.features.clone().unwrap();
-//                     if df
-//                         .iter()
-//                         .filter(|f| cvf.contains(f))
-//                         .collect::<Vec<_>>()
-//                         .len()
-//                         == df.len()
-//                     {
-//                         return true;
-//                     }
-//                 }
-//             }
-//         }
-//         false
-//     }
-// }
